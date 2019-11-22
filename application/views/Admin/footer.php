67d3fb84a4965b496a533f5fe04d7df96ebc63cf
technothinksup <project.technothinksup@gmail.com>
2019-11-22 17:32:28 +0530

    footer add version

--- application/views/Admin/footer.php
+++ application/views/Admin/footer.php
@@ -2,6 +2,6 @@
   <strong>Copyright &copy; 2019-2020 Universal Digital System.</strong>
   All rights reserved.
   <div class="float-right d-none d-sm-inline-block">
-    <b>Version</b>
+    <b>Version</b> 1.0
   </div>
 </footer>
