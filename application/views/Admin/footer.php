8f086558c3612530e039f19f9f4ed61e0e34a2c2
technothinksup <project.technothinksup@gmail.com>
2019-11-22 17:31:39 +0530

    footer change

--- application/views/Admin/footer.php
+++ application/views/Admin/footer.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
-  <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+  <strong>Copyright &copy; 2019-2020 Universal Digital System.</strong>
   All rights reserved.
   <div class="float-right d-none d-sm-inline-block">
-    <b>Version</b> 3.0.0-rc.5
+    <b>Version</b>
   </div>
 </footer>
